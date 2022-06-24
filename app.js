417469e36f2cb0e2b59ae0bb5f47401d573be2de
Hamza odowa <hamsenasir97@gmail.com>
2022-06-24 02:19:02 +0100

    add bcrypt to password level3

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -24,11 +25,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-// added the encryption package to userSchema the plugin, and specifes to encypt on password
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,17 +41,19 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username, // < this get the username data from the form in register.ejs
-    password: req.body.password, //< this get the password data from the form in register.ejs
-  });
-
-  newUser.save(function (err) {
-    if (err) {
-      console.log();
-    } else {
-      res.render("secrets"); // only render the sercets page unless the user is login in. (from the register route)
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username, // < this get the username data from the form in register.ejs
+      password: hash, //< this get the password data from the form in register.ejs
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log();
+      } else {
+        res.render("secrets"); // only render the sercets page unless the user is login in. (from the register route)
+      }
+    });
+    // Store hash in your password DB.
   });
 });
 
@@ -69,10 +67,13 @@ app.post("/login", function (req, res) {
     } else {
       if (foundUser) {
         // else if the foundUSer does exit in the DB 👇
-        if (foundUser.password === password) {
-          // and if that user's password exactly matches the this password
-          res.render("secrets"); // then they pass authentication and can access their secets page(login successfull)
-        }
+        // Load hash from your password DB.
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            // and if that user's password exactly matches the this password
+            res.render("secrets"); // then they pass authentication and can access their secets page(login successfull)
+          }
+        });
       }
     }
   });
